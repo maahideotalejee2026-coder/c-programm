e71d43c84a54d12af0fc3438ad506fcbec162d0f
Maahi Deotale <maahideotalejee2026@gmail.com>
2026-09-24 22:49:36 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    float height;
+    double salary;
+    char grade;
+    printf("enter age:");
+    scanf("%d",&age);
+    printf("enter height:");
+    scanf("%f",&height);
+    printf("enter salary:");
+    scanf("%lf",&salary);
+    printf("enter grade:");
+    scanf(" %c",&grade);
+    printf("---Details---\n");
+    printf("Age=%d\n",age);
+    printf("Height=%.2f\n",height);
+    printf("salary=%.2lf\n",salary);
+    printf("grade=%c\n",grade);
+    return 0;
+}
