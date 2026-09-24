87ec8fcfc81febd7c2fb14216ec97935f71ad7f1
Maahi Deotale <maahideotalejee2026@gmail.com>
2026-09-25 00:20:53 +0530

    single line comment
    Refactored the main function to include variable declarations and a sum calculation.

---
+++ single_linecmt.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main()
+{
+    //THis is a single-line comment
+  
+  int a = 10; 
+  int b = 20;
+/* This is a multi-line comment. 
+It can span across multiple lines. */
+int sum = a + b;
+// Displaying the sum 
+printf("Sum = %d\n", sum);
+return 0;
+}
